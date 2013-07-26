8660e3dc0cd7c6d0c55c97970f78139ea077d68c
Bryan Lam <Bryan.S.Lam@gmail.com>
2013-07-26 14:32:11 -0700

    FREQUENT COMMITS MOTHERFUCKER cleared up main menu selection

--- Game.py
+++ Game.py
@@ -1,15 +1,32 @@
 #Initial Setup
+import sys
 
 #Game intro
+#Initial warning
+print "\nThis game doesn't save and one death will restart the game enter 0 at any point in the game to bring up the menu\n"
 
-print """You are inside a tower with 7 floors, each floor containing numerous monsters, hidden treasures, 
+#Main Menu
+menuSelection = 0
+while(menuSelection != '1' and menuSelection != '2'):
+	print "Press 1 to start a new game or 2 to quit"
+	menuSelection = raw_input('>')
 
-and a floor boss at each level.  Only by defeating each floor boss will you be allowed to move onto
+if(menuSelection == '1'):
+	print """You are inside a tower with 7 floors, each floor containing numerous monsters, hidden treasures, 
 
-the next floor.  The rooms are all open space in a 4x4 grid where each turn you'll be asked which direction
+	and a floor boss at each level.  Only by defeating each floor boss will you be allowed to move onto
 
-you'd like to move to.  Each floor space can either contain a treasure chest, a shop, or a monster.  The floor
+	the next floor.  The rooms are all open space in a 4x4 grid where each turn you'll be asked which direction
 
-boss will only appear after clearing every monster on the floor on the top left corner of the grid and a 
+	you'd like to move to.  Each floor space can either contain a treasure chest, a shop, or a monster.  The floor
 
-notification will let you know when the boss appears."""
+	boss will only appear after clearing every monster on the floor on the top left corner of the grid and a 
+
+	notification will let you know when the boss appears."""
+
+elif(menuSelection == '2'):
+	sys.exit()
+	
+
+
+
