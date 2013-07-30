85b994d2a40c8da367af2730acedc9e2225963fe
Bryan Lam <Bryan.S.Lam@gmail.com>
2013-07-30 10:10:34 -0700

    Initial attacking algorithim added -- untested

--- Character.py
+++ Character.py
@@ -8,7 +8,23 @@ class Player():
 		self.inventory = []
 		
 		#make stats more job specific later on
-		self.strength = randint(10,20)
+		self.strength = randint(5,10)
 		self.dexterity = randint(5,10)
 		self.intelligence = randint(5,10)
 		self.magicPoints = 5 + self.intelligence
+	
+#Dungeons and dragons mechanics	
+	def attack(self, monsterDexterity, monsterArmor):
+		initialRoll = randint(0,20)
+		rollToHit = initialRoll + self.dexterity
+		critical = false
+		if(initialRoll = 20):
+			critical = true
+		if(rollToHit >= monsterDexterity):
+			#If hit commence damage sequence
+			if(critical == true):
+				return randint(0,6) + randint(0,6) + self.strength - monsterArmor
+			else:
+				return randint(0,6) + self.strength - monsterArmor
+		else:
+			print "Missed!"
