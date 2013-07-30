05e912efc1d1f6776aecc7d6232e7142555fc1a2
Bryan Lam <Bryan.S.Lam@gmail.com>
2013-07-30 09:34:59 -0700

    Fixed up initial character stat calculation and added more to start of game

--- Character.py
+++ Character.py
@@ -3,12 +3,12 @@ from random import randint
 class Player():
 	def __init__(self, job):
 		self.job = job
-		self.hitPoints = randint(10,20)
+		self.hitPoints = randint(21,30)
 		self.gold = 0
 		self.inventory = []
 		
 		#make stats more job specific later on
 		self.strength = randint(10,20)
-		self.dexterity = randint(10,20)
-		self.intelligence = randint(10,20)
-		self.magicPoints = 10 + intelligence
+		self.dexterity = randint(5,10)
+		self.intelligence = randint(5,10)
+		self.magicPoints = 5 + self.intelligence
--- Game.py
+++ Game.py
@@ -1,15 +1,17 @@
 #Initial Setup
 import sys
+from random import randint
 from Tower import Floor
+from Character import Player
 #Game intro
 #Initial warning
-print "\nThis game doesn't save and one death will restart the game enter 0 at any point in the game to bring up the menu\n"
+#print "\nThis game doesn't save and one death will restart the game enter 0 at any point in the game to bring up the menu\n"
 
 #Main Menu
 menuSelection = 0
 while(menuSelection != '1' and menuSelection != '2'):
 	print "Press 1 to start a new game or 2 to quit"
-	menuSelection = raw_input('>')
+	menuSelection = raw_input('> ')
 
 #begin game
 if(menuSelection == '1'):
@@ -25,10 +27,21 @@ if(menuSelection == '1'):
 
 	notification will let you know when the boss appears.\n"""
 	
-	
-	floor1 = Floor(1)
-	
-	
+	characterSelection = 0
+	while(characterSelection != '1'):
+		print "Select your class:"
+		print "1: Warrior"
+		characterSelection = raw_input('> ')
+	if(characterSelection == '1'):
+		mainCharacter = Player("Warrior")
+		print "These are your character stats: "
+		print "Strength:", mainCharacter.strength
+		print "Dexterity:", mainCharacter.dexterity
+		print "Intelligence:", mainCharacter.intelligence
+		print "HP:", mainCharacter.hitPoints
+		print "MP:", mainCharacter.magicPoints
+		
+		
 elif(menuSelection == '2'):
 	sys.exit()
 	
