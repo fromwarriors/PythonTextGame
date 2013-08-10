e37536a23d9a5a09ea7068fb41fe2bb8c8880dd2
Michael Oselskyi <oselskyi@gmail.com>
2013-08-10 15:44:35 -0700

    Update Tower.py
    -pep8 optimized
    -python 3 support

--- Tower.py
+++ Tower.py
@@ -6,7 +6,7 @@
 
 #Move set 1-4 = first row 5-8 = second row 9-12 = third row 12-15 = fourth row
 from random import randint
-from Character import Player
+from Character import Player # unused
 
 class Floor(object):
 #	PROPERTIES
@@ -15,20 +15,20 @@ class Floor(object):
 #	mounsterCount
 #	floorNumber
 
-	def __init__(self, floorNumber, character):
-		#Map setup
-		self.generatedMap = [[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4]]
-		self.floorNumber = floorNumber
-		if(floorNumber == 1):
-			self.monsterCount = 2
-		
-		#monster placement
-		for i in range (0, self.monsterCount):
-			row = randint(0,4)
-			column = randint(0,4)
-			while(self.generatedMap[row][column] == column):
-				row = randint(0,4)
-				column = randint(0,4)
-			self.generatedMap[row][column] = 'M'
-		
-		
+    def __init__(self, floorNumber, character):
+        #Map setup
+        self.generatedMap = [[1,2,3,4],[1,2,3,4],[1,2,3,4],[1,2,3,4]]
+        self.floorNumber = floorNumber
+        if(floorNumber == 1):
+            self.monsterCount = 2
+        
+        #monster placement
+        for i in range (0, self.monsterCount):
+            row = randint(0,4)
+            column = randint(0,4)
+            while(self.generatedMap[row][column] == column):
+                row = randint(0,4)
+                column = randint(0,4)
+            self.generatedMap[row][column] = 'M'
+        
+        
